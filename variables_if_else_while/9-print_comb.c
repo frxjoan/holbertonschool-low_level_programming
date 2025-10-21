836d4b461ef86cfe59cc3355d44f55f5187fc151
frxjoan <12224@holbertonstudents.com>
2025-10-21 14:10:03 +0200

    9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,11 +7,14 @@
 int main(void)
 {
 int i;
-for (i = '0'; i <= '8'; ++i)
+for (i = '0'; i <= '9'; ++i)
+if (i != '9')
+{
 {
 putchar(i);
 putchar(',');
 putchar(' ');
+}
 }
 putchar('9');
 return (0);
