a7ad573eb13ceca6b7f5589ee135d643932fb2f5
frxjoan <12224@holbertonstudents.com>
2025-10-22 16:58:05 +0200

    j

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,9 +15,17 @@ for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
+if (i * j < 9)
+{
 _putchar(i * j + '0');
 _putchar(',');
 _putchar(' ');
+}
+else
+{
+_putchar(i * j / 10 + '0');
+_putchar(i * j % 10 + '0');
+}
 }
 _putchar('\n');
 }
