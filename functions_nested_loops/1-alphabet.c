300ab461c014a50be9fd127032bfbdbeca566071
frxjoan <12224@holbertonstudents.com>
2025-10-22 09:55:02 +0200

    change program into function

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 char c;
 for (c = 'a'; c <= 'z'; c++)
@@ -12,5 +12,4 @@ for (c = 'a'; c <= 'z'; c++)
 _putchar(c);
 }
 _putchar('\n');
-return (0);
 }
