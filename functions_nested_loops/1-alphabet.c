8eb0aed2e8eb66b3ba7aa34eda032d07f7918be9
frxjoan <12224@holbertonstudents.com>
2025-10-22 09:23:58 +0200

    function 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int main(void);
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+putchar('\n');
+return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 
 int _putchar(char a);
+void print_alphabet(void);
 #endif
