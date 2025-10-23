62f4b304a99a28b76805aa7bc54630f8d81cb361
frxjoan <12224@holbertonstudents.com>
2025-10-23 14:18:15 +0200

    print

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_to_98 - Short description, single line
@@ -9,21 +10,21 @@
  */
 void print_to_98(int n)
 {
-    if (n != 98 && n < 98)
+    if (n < 98)
     {
-        _putchar(n + '0');
+        printf('%d, ', n);
         n = n + 1;
         print_to_98(n);
     }
-    else if (n != 98 && n > 98)
+
+    else if (n > 98)
     {
-        _putchar(n + '0');
+        printf('%d, ', n);
         n = n - 1;
         print_to_98(n);
     }
     else
     {
-        _putchar(n / 10 + '0');
-        _putchar(n % 10 + '0');
+        printf('%d', n);
     }
 }
