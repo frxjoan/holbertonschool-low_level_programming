ca35e899dd6e3a60e79f68a01551a71ec1b3231c
frxjoan <12224@holbertonstudents.com>
2025-10-23 14:29:48 +0200

    h

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,21 +10,20 @@
  */
 void print_to_98(int n)
 {
-    if (n < 98)
-    {
-        printf("%d, ", n);
-        n = n + 1;
-        print_to_98(n);
-    }
-
-    else if (n > 98)
-    {
-        printf("%d, ", n);
-        n = n - 1;
-        print_to_98(n);
-    }
-    else
-    {
-        printf("%d\n", n);
-    }
+	if (n < 98)
+	{
+		printf("%d, ", n);
+		n = n + 1;
+		print_to_98(n);
+	}
+	else if (n > 98)
+	{
+		printf("%d, ", n);
+		n = n - 1;
+		print_to_98(n);
+	}
+	else
+	{
+		printf("%d\n", n);
+	}
 }
