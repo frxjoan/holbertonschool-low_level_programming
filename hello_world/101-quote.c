2807920fdfe71943843218e2f52965170f23a471
frxjoan <12224@holbertonstudents.com>
2025-10-24 12:07:50 +0200

    \n

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -9,6 +9,6 @@ int main(void)
 {
 	write(2, "and that piece of art", strlen("and that piece of art"));
 	write(2, " is useful\" - ", strlen(" is useful\" - "));
-	write(2, "Dora Korpar, 2015-10-19", strlen("Dora Korpar, 2015-10-19"));
+	write(2, "Dora Korpar, 2015-10-19\n", strlen("Dora Korpar, 2015-10-19\n"));
 	return (1);
 }
