44cfb89f3abd5c0632670c80ee58caf2987e58bb
frxjoan <12224@holbertonstudents.com>
2025-10-24 12:06:14 +0200

    j

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,8 +1,14 @@
 #include <unistd.h>
 #include <string.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    write (2, "and that piece of art is useful\"", strlen)
-    return 1
+	write(2, "and that piece of art", strlen("and that piece of art"));
+	write(2, " is useful\" - ", strlen(" is useful\" - "));
+	write(2, "Dora Korpar, 2015-10-19", strlen("Dora Korpar, 2015-10-19"));
+	return (1);
 }
