77492c6760862de1a81227cc2b21abdd74a7f137
frxjoan <12224@holbertonstudents.com>
2025-10-27 11:54:35 +0100

    g

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - Short description, single line
+ * @n: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i = n; i++)
+	{
+		_putchar('_');
+	}
+	_puchar('\n');
+}
