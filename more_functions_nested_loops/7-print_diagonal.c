016d33a9bc580b018b098c580c9ec515f6aef45e
frxjoan <12224@holbertonstudents.com>
2025-10-27 13:50:23 +0100

    uu

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -14,7 +14,7 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		_putchar('/');
+		_putchar('\\');
 		_putchar('\n');
 		for (i = 1; i <= n; i++)
 		{
@@ -22,7 +22,7 @@ void print_diagonal(int n)
 			{
 				_putchar(' ');
 			}
-			_putchar('/');
+			_putchar('\\');
 			_putchar('\n');
 		}
 	}
