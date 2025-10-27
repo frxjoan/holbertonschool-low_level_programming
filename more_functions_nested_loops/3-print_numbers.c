50ae681a2c48bc2de0def99ceac6aa2ab8bf07be
frxjoan <12224@holbertonstudents.com>
2025-10-27 11:00:53 +0100

    mul

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+}
