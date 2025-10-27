329d3ee3a05d99b29aa68ea2b18a7209d6eafe08
frxjoan <12224@holbertonstudents.com>
2025-10-27 16:03:43 +0100

    k

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - Short description, single line
+ * @size: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+void print_triangle(int size)
+{
+    int i;
+    int j;
+
+    for (i = 1; i <= size; i++)
+    {
+        for (j = 1; j <= size; j++)
+        {
+            if (j > size - i)
+            {
+                _putchar('#');
+            }
+            else
+            {
+                _putchar(' ');
+            }
+        }
+		_putchar('\n');		
+		
+    }
+}
