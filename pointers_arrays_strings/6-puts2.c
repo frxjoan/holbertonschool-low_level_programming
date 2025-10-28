459a49301e029aa9b40fa99feddcabfa24e61747
frxjoan <12224@holbertonstudents.com>
2025-10-28 13:35:52 +0100

    ok

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* puts2 - Short description, single line
+* @str: Description of parameter x
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n')
+}
