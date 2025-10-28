b28a127c0a2ec548069f9510238e017a44a03c19
frxjoan <12224@holbertonstudents.com>
2025-10-28 09:41:01 +0100

    pointer

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - Short description, single line
+ * @*n: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+void reset_to_98(int *n)
+{
+	int *n = &n;
+	*n = 98;
+}
