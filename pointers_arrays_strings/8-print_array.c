de911ecdb83bc016bcaac06ae1ac7067c367c993
frxjoan <12224@holbertonstudents.com>
2025-10-28 15:43:04 +0100

    l

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_array - Short description, single line
  * @a: Description of parameter x
@@ -12,5 +13,10 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i <= n; i++)
-		printf("%d, ", a[i]);
+	{
+		if (i != n)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
 }
