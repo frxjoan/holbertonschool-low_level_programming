df48349f6767f3a0e6ba22a7502f990fe83a239b
frxjoan <12224@holbertonstudents.com>
2025-10-28 10:18:03 +0100

    arg

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - Short description, single line
+ * @str: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		_putchar(str[i] + '0')
+
+	return (0);
+}
