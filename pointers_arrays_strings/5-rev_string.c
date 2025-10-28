bb94dc1a02f8cb5033ebd8945eca94c4a2bf801b
frxjoan <12224@holbertonstudents.com>
2025-10-28 11:28:48 +0100

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - Short description, single line
+ * @s: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+	for (j = i - 1; j >= 0; j--)
+			_putchar(s[j]);
+	_putchar('\n');
+}
