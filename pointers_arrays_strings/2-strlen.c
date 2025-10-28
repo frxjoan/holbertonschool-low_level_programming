b806693484c26669ea5741fb4b5408f36b6f81a0
frxjoan <12224@holbertonstudents.com>
2025-10-28 10:04:11 +0100

    o

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - Short description, single line
+ * @s: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	while (*s[i] != '\0')
+	{
+		i++
+	}
+	return (0);
+}
