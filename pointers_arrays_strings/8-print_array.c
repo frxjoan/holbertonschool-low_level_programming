dc321cccba914a32b04225c20f5f0c96cc485f28
frxjoan <12224@holbertonstudents.com>
2025-10-28 15:40:17 +0100

    l

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_array - Short description, single line
+ * @a: Description of parameter x
+ * @n: Description of parameter x
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+		printf("%d, ", a[i]);
+}
