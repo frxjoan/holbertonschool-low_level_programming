7328b62d65dc1a1c96cedab46c84f38897d444fa
frxjoan <12224@holbertonstudents.com>
2025-10-28 10:38:29 +0100

    hmm

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - Short description, single line
+ * @s: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+		i++;
+	for (j = i; j >= 0; j--)
+		_putchar(s[j]);
+}
