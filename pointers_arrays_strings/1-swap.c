3dcf9029f4b988a0c0e8ea261b963329fce45bcd
frxjoan <12224@holbertonstudents.com>
2025-10-28 09:47:09 +0100

    ab=ba

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - Short description, single line
+ * @a: Description of parameter x
+ * @b: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+void swap_int(int *a, int *b)
+{
+    *a, *b = *b, *a;
+}
