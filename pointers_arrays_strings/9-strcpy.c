de2e5b3d29c252b9cecf23e96acb81ea9e27910a
frxjoan <12224@holbertonstudents.com>
2025-10-28 16:07:40 +0100

    maybe

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcpy - Short description, single line
+ * @dest: Description of parameter x
+ * @src: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src
+}
