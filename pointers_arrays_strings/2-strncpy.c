fcd746a1880733b2e77b6189347dffcee409ebe3
frxjoan <12224@holbertonstudents.com>
2025-10-29 15:10:27 +0100

    trop de dest

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strncpy - Short description, single line
+ * @dest: Description of parameter x
+ * @src: D
+ * @n: D
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *odest = dest;
+	char *bdest;
+	int i;
+
+	for (i = 0; i < n && *src != '\0'; i++)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	bdest = dest;
+	if (n >= i)
+	{
+		for (i = i; i <= n && *dest != '\0'; i++)
+		{
+			*dest = '\0';
+			dest++;
+		}
+	}
+	return odest;
+}
