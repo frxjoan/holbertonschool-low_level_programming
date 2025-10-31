cbec210c6ff3fb045db122e8a36b99078ba88ee8
frxjoan <12224@holbertonstudents.com>
2025-10-31 12:00:27 +0100

    poisson de cacaa

---
+++ more_functions_nested_loops/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - Short description, single line
+ * @p: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+char *rot13(char *p)
+{
+	*pc = p;
+	int i;
+	char alphabet1[] = "abcdefghijklmABCDEFGHIJKLM"
+	char alphabet2[] = "nopqrstuvwxyzNOPQRSTUVWXYZ"
+	
+	while (*p != '\0')
+	{
+		for (i = 0; i <= 25; i++)
+		{
+			if (*s == alphabet1[i] || *s == alphabet2[i])
+			{
+				*s = alphabet2[i] || *s = alphabet1[i];
+			}
+		}
+	}
+	return (pc);
+}
