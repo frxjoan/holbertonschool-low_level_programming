88d7dcaa1e64289fa3c534d19a77f0d8951ae159
frxjoan <12224@holbertonstudents.com>
2025-10-31 09:13:51 +0100

    rework atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,16 +11,30 @@ int _atoi(char *s)
 {
 	int res = 0;
 	int n = 1;
+	int INT_MAX = 2147483647
+	int INT_MAX = (-INT_MAX - 1)
 
 	while (*s != '\0')
 	{
-		if (*s >= '0' && *s <= '9')
-		{
-			res *= 10;
-			res += *s - 48;
-		}
-		else if (*s == '-')
+		if (*s == '-')
 			n = n * -1;
+		else if (*s >= '0' && *s <= '9')
+		{
+			int num = *s - '0';
+
+			if (n == 1)
+			{
+				if (res > (INT_MAX - digit) / 10)
+					return (INT_MAX);
+				res = res * 10 + num;
+			}
+			else
+			{
+				if (res > (-(INT_MIN + digit)) / 10)
+					return (INT_MIN);
+				res = res * 10 - num;
+			}
+		}
 		else if (res != 0)
 			break;
 		s++;
