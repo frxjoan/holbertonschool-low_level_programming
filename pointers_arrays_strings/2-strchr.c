43b42f5f4e022f11e1e8efef2bdedc719317c414
frxjoan <12224@holbertonstudents.com>
2025-11-03 10:18:15 +0100

    exo2 try1

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - Short description, single line
+ * @s: Description of parameter x
+ * @c: D
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+char *_strchr(char *s, char c)
+{
+	char *cpys = s;
+
+	while (*cpys != '\0')
+	{
+		if (*cpys == c)
+			return (cpys);
+		cpys++;
+	}
+	return ('\0');
+}
