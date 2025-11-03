7461b1571df1dfa28535f42b47c756d8c8a29e48
frxjoan <12224@holbertonstudents.com>
2025-11-03 09:54:27 +0100

    changed <= n to <n

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -12,11 +12,12 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
+	char *p = s;
 
-	while (i <= n)
+	while (i < n)
 	{
-		*s = b;
+		s[i] = b;
 		i++;
 	}
-	return (s);
+	return (p);
 }
