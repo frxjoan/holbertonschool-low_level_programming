3427eead82b59827213f1ad346ca6ebaa2c844bc
frxjoan <12224@holbertonstudents.com>
2025-11-03 20:58:55 +0100

    1 clear

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - Short description, single line
+ * @s: Description of parameter x
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
