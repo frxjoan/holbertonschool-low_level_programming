999685aacb2961317b378c47785d8e8264025e44
frxjoan <12224@holbertonstudents.com>
2025-11-04 18:39:16 +0100

    argc, argv ex0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* This is the preferred style for multi-line
+* Description:  A column of asterisks on the left side,
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+    return (0);
+}
