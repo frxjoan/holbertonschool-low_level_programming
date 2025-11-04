24fa0da8b97a6c325f77123b0f992081f285fc7f
frxjoan <12224@holbertonstudents.com>
2025-11-04 09:57:54 +0100

    fixing betty

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -2,13 +2,16 @@
 /**
  * factorial - Short description, single line
  * @n: Description of parameter x
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
  */
 int factorial(int n)
 {
 	if (n == 0)
 		return (1);
-	else if (n < 0)
+	if (n < 0)
 		return (-1);
-	else
+	else if
 		return (n * factorial(n - 1));
 }
