cf293c36c08cd8ec070392d2bc58061f3783c376
frxjoan <12224@holbertonstudents.com>
2025-11-04 11:32:18 +0100

    git fix

--- recursion/5-sqrt.c
+++ recursion/5-sqrt.c
@@ -4,18 +4,22 @@
  * @n: Description of parameter x
  * Return: un int
  */
-int _sqrt(int n, int i)
+int _sqrt_check(int n, int i)
 {
 	if (i * i == n)
 		return (i);
-	if (i * i > n)
+	if (i * i < 0)
 		return (-1);
-	return (_sqrt(n, i + 1));
+	return (_sqrt_check(n, i + 1));
 }
-
- int _sqrt_recursion(int n)
+/**
+ * _sqrt_recursion - s
+ * @n: s
+ * Return: s
+ */
+int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (_sqrt(n, 1));
+	return (_sqrt_check(n, 1));
 }
