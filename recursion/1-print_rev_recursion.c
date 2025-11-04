08fc91829e82d19a87a49a71227958dde3c2b7d3
frxjoan <12224@holbertonstudents.com>
2025-11-04 09:46:22 +0100

    hel na

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -5,15 +5,8 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int i;
-	char *p = s;
-
-	if (*s != '\0')
-	{
-		_print_rev_recursion(s + 1);
-	}
-	for (i = *s; i >= *p; i--)
-	{
-		_putchar(*s);
-	}
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
