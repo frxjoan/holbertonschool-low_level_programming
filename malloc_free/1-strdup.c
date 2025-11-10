b7e46dbfb0d5acbd2021b9276f7b3d82ceb45d64
frxjoan <12224@holbertonstudents.com>
2025-11-10 10:53:05 +0100

    syntaxique

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - Short description, single line
+ * @str: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+char *_strdup(char *str)
+{
+	unsigned long int i;
+	char *ptr;
+
+	ptr = malloc(sizeof(str) * sizeof(char));
+	if (str == NULL)
+		return (0);
+	for (i = 0; i <= sizeof(str) + 1; i++)
+		ptr[i] = str[i];
+	return (ptr);
+}
