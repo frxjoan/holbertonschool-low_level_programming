cdcfc571a5e2bccd1e8af4f446ed4a4ab8c1ddea
frxjoan <12224@holbertonstudents.com>
2025-11-12 17:28:18 +0100

    oe

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - Short description, single line
+ * @nmemb: Description of parameter x
+ * @size: D
+ * Description: Longer description of the function)?
+ * Return: Description of the returned value
+ */
+void _calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	int i, total = size * nmemb;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	if (nmemb != 0 && (total / nmemb) != size)
+		return (NULL);
+	p = malloc(size * nmemb);
+		if (p == NULL)
+			return (NULL);
+	for (i = 0; i < total; i++)
+		p[i] = 0;
+	return (p);
+}
