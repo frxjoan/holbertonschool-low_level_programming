33979de6e892c05bd23bc183123c67a42f14cadb
frxjoan <12224@holbertonstudents.com>
2025-11-13 16:07:06 +0100

    ok

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,40 @@
 #include <string.h>
 #include <stdlib.h>
 /**
+ * _strlen - Short description, single line
+ * @str: Description of parameter x
+ * Return: fzifhzpi
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+ * _strdup - Short description, single line
+ * @str: Description of parameter x
+ * Return: fzifhzpi
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+
+	ptr = malloc((_strlen(str) + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i <= _strlen(str); i++)
+		ptr[i] = str[i];
+	ptr[i] = '\0';
+	return (ptr);
+}
+/**
  * new_dog - Short description, single line
  * @name: D
  * @age: D
@@ -15,8 +49,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dogo = malloc(sizeof(dog_t));
 	if (dogo == NULL)
 		return (NULL);
-	dogo->name = strdup(name);
+	dogo->name = _strdup(name);
 	dogo->age = age;
-	dogo->owner = strdup(owner);
+	dogo->owner = _strdup(owner);
 	return (dogo);
 }
