39f324c5727012167cf0d2b889f48cfda172b79e
frxjoan <12224@holbertonstudents.com>
2025-11-13 11:29:13 +0100

    pls

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -23,10 +23,14 @@ char *_strdup(char *str)
 	int i;
 	char *ptr;
 
+	if (str == NULL)
+		return (NULL);
+
 	ptr = malloc((_strlen(str) + 1) * sizeof(char));
-	if (ptr == NULL || str == NULL)
+	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i <= _strlen(str); i++)
 		ptr[i] = str[i];
+	ptr[i] = '\0';
 	return (ptr);
 }
