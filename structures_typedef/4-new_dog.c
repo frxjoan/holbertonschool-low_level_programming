7337c5ba2ad7def751ac81ae67a110f775fb6cf7
frxjoan <12224@holbertonstudents.com>
2025-11-13 15:23:05 +0100

    4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * print_dog - Short description, single line
+ * @name: D
+ * @age: D
+ * @owner: D
+ * Return: Description of the returned value
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t = (name, age, owner);
+	return (dog_t);
+}
