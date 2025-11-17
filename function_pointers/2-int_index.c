3a3e6d6324025fdf4e4afe81ddbfda8303d1dc78
frxjoan <12224@holbertonstudents.com>
2025-11-17 10:26:32 +0100

    2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * int_index - Short description, single line
+ * @array: D
+ * @size: D
+ * @cmp: D
+ * Return: Description of the returned value
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]))
+			return (i);
+	return (-1);
+}
