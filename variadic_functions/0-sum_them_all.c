fc96a26c62a21d77db722d3f166e45de0a053017
frxjoan <12224@holbertonstudents.com>
2025-11-18 10:50:18 +0100

    sum_them_all

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - Short description, single line
+ * @n: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list arg;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(arg, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(arg, int);
+	va_end(arg);
+	return (sum);
+}
