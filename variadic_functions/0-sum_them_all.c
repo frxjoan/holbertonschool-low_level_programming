2d1bc73a7b5f9b85de4cb65509ce9f3cd156f0b8
frxjoan <12224@holbertonstudents.com>
2025-11-19 10:31:09 +0100

    betty bug ?

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -13,9 +13,13 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
+
 	va_start(arg, n);
+
 	for (i = 0; i < n; i++)
 		sum += va_arg(arg, int);
+
 	va_end(arg);
+
 	return (sum);
 }
