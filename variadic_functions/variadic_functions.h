142c5c37d67677cc079acbb39ccbf055c8489a1b
frxjoan <12224@holbertonstudents.com>
2025-11-19 10:44:18 +0100

    comment

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -7,8 +7,13 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-typedef struct
+/**
+ * struct printev - Short description, single line
+ * @type: Description of parameter x
+ * @form: Description of parameter x
+ * Return: Description of the returned value
+ */
+typedef struct printev
 {
 	char type;
 	void (*form)(va_list arg, int *bool);
