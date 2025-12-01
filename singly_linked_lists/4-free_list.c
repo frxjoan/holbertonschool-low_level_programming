628bcf030fb9917cfc214d5fa5230c60c602630e
frxjoan <12224@holbertonstudents.com>
2025-12-01 13:50:37 +0100

    free4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_list - singly linked list
+ * @head: string - (malloc'ed string)
+ * Return: size
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	if (head != NULL)
+	{
+		while (tmp != NULL)
+		{
+			tmp = head->next;
+			free(head->str);
+			free(head);
+			head = tmp;
+		}
+	}
+}
