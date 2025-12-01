73f52720a0afbc3bef48a7fae08342ffeff355a1
frxjoan <12224@holbertonstudents.com>
2025-12-01 13:29:01 +0100

    3

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * add_node_end - singly linked list
+ * @head: string - (malloc'ed string)
+ * @str: string - (malloc'ed string)
+ * Return: size
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *tmp = *head;
+	int i, lenn = 0;
+	list_t *neuneu = malloc(sizeof(list_t));
+
+	if (neuneu == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		lenn += 1;
+	neuneu->str = strdup(str);
+	neuneu->len = lenn;
+	neuneu->next = NULL;
+	if (*head == NULL)
+		*head = neuneu;
+	else
+	{
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = neuneu;
+	}
+	return (neuneu);
+}
